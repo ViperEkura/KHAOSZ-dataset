c4ce882d56d71aa875bde00f4801bcdc79103111
ViperEkura <3081035982@qq.com>
2025-08-01 17:28:23 +0800

    feat: 添加 Magpie-Pro-300K 数据集的 SFT 处理脚本

---
+++ sft_magpie-pro-300k.py
@@ -0,0 +1,30 @@
+# HuggingFaceTB/Magpie-Pro-300K-Filtered-H4
+from datasets import load_dataset
+from utils import process_dataset
+
+
+def process_func(input_dict: dict):
+    conversations = input_dict["conversations"]
+    assert len(conversations) % 2 == 0
+    n = len(conversations) // 2
+    examples = []
+    
+    for i in range(n):
+        user_msg = conversations[2*i]["value"]
+        assistant_msg = conversations[2*i+1]["value"]
+        examples.append({
+            "query": user_msg,
+            "response": assistant_msg
+        })
+    
+    return examples
+
+
+if __name__ == "__main__":
+    dataset = load_dataset("HuggingFaceTB/Magpie-Pro-300K-Filtered-H4")
+    process_dataset(
+        dataset_dict=dataset,
+        output_subdir="belle-sft",
+        process_func=process_func,
+        split_name="train-sft"
+    )
