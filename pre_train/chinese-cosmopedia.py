f7200ee4cf21857192246db3b48e9b09a0014ace
ViperEkura <3081035982@qq.com>
2025-08-06 09:58:15 +0800

    feat(pre_train): 更新 Chinese Cosmopedia 数据集处理脚本

--- pre_train/chinese-cosmopedia.py
+++ pre_train/chinese-cosmopedia.py
@@ -2,17 +2,15 @@ from datasets import load_dataset
 from modules.utils import process_dataset
 
 if __name__ == "__main__":
-    max_chunk_num = 10
     chunk_size = 1000000
     
     dataset = load_dataset(
         "opencsg/chinese-cosmopedia", 
-        data_files={"train": [f"data/0000{i}.parquet" for i in range(5)]}
+        data_files={"train": [f"data/000{i:02d}.parquet" for i in range(25)]}
     )
     
     process_dataset(
         dataset_dict=dataset,
         output_subdir="chinese-wiki-pretrain",
-        max_chunk_num=max_chunk_num,
         chunk_size=chunk_size,
     )
